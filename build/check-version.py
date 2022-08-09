96c50d6d304ff775993d41547444bdd9f1037564
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2022-08-09 15:35:48 +1200

    Improve error messaging of check-version.py

--- build/check-version.py
+++ build/check-version.py
@@ -26,13 +26,20 @@ class SemVer:
                self.minor <= other.minor and \
                self.patch <= other.patch
 
+    def __repr__(self):
+        return f"SemVer('{self.major}.{self.minor}.{self.patch}"
+
+    def __str__(self):
+        return f"{self.major}.{self.minor}.{self.patch}"
+
 
 if __name__ == '__main__':
     format_help_spec = "In format MAJOR.MINOR.PATCH eg. 1.2.3"
     parser = argparse.ArgumentParser(description="""Checks new_version is newer than current_version.\n
     Exit code(0) - new_version is newer than current_version.\n
-    Exit code(1) - new_version is older than current_version.\n
-    Exit code(2) - new_version is the same as current_version.
+    Exit code(1) - Error thrown in the script
+    Exit code(2) - new_version is older than current_version.\n
+    Exit code(3) - new_version is the same as current_version.
     """)
 
     parser.add_argument('new_version', type=str, help=f"The version you are trying to update to. {format_help_spec}")
@@ -43,8 +50,11 @@ if __name__ == '__main__':
     current_version = SemVer(args.current_version)
 
     if new_version > current_version:
+        print(f"Success: {new_version} > {current_version}")
         exit(0)  # Success
     if new_version < current_version:
-        exit(1)
-    if new_version == current_version:
+        print(f"Invalid: {new_version} < {current_version}")
         exit(2)
+    if new_version == current_version:
+        print(f"Invalid: {new_version} == {current_version}")
+        exit(3)
