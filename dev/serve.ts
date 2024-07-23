2504b3615a9114449b2f15f1971db27c8359ca3d
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2024-07-24 11:20:02 +1200

    Arrange library more like traditional library with lib directory

--- dev/serve.ts
+++ dev/serve.ts
@@ -2,7 +2,7 @@ import {createApp} from 'vue';
 import Dev from './serve.vue';
 // To register individual components where they are used (serve.vue) instead of using the
 // library as a whole, comment/remove this import and it's corresponding "app.use" call
-import GeoVisualisationComponents from '@/entry.esm';
+import GeoVisualisationComponents from '../lib/main';
 
 const app = createApp(Dev);
 
