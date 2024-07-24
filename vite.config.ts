30569ecc04e4142e0e339037edfdebe3d3f5e9df
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2024-07-24 16:14:23 +1200

    Get build with type declarations working

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,43 @@
-// vite.config.js
-import {resolve} from 'path'
 import {defineConfig} from 'vite'
-import {fileURLToPath} from "node:url";
-import {viteStaticCopy} from "vite-plugin-static-copy";
 import vue from '@vitejs/plugin-vue'
+import typescript2 from 'rollup-plugin-typescript2';
+import dts from "vite-plugin-dts";
+import {viteStaticCopy} from 'vite-plugin-static-copy'
+import sassDts from 'vite-plugin-sass-dts'
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+import path from "path";
+import {fileURLToPath} from "node:url";
 
 export default defineConfig({
   plugins: [
     vue(),
+    cssInjectedByJsPlugin(),
+    dts({
+      insertTypesEntry: true,
+    }),
+    sassDts(),
+    cssInjectedByJsPlugin(),
     viteStaticCopy({
       targets: [
-          {src: './node_modules/cesium/Build/Cesium/Assets', dest: './'},
-          {src: './node_modules/cesium/Build/Cesium/ThirdParty', dest: './'},
-          {src: './node_modules/cesium/Build/Cesium/Widgets', dest: './'},
-          {src: './node_modules/cesium/Build/Cesium/Workers', dest: './'},
+        {src: './node_modules/cesium/Build/Cesium/Assets', dest: './'},
+        {src: './node_modules/cesium/Build/Cesium/ThirdParty', dest: './'},
+        {src: './node_modules/cesium/Build/Cesium/Widgets', dest: './'},
+        {src: './node_modules/cesium/Build/Cesium/Workers', dest: './'},
       ]
     }),
+    typescript2({
+      check: false,
+      include: ["lib/components/**/*.vue"],
+      tsconfigOverride: {
+        compilerOptions: {
+          outDir: "dist",
+          sourceMap: true,
+          declaration: true,
+          declarationMap: true,
+        },
+      },
+      exclude: ["vite.config.ts"],
+    })
   ],
   resolve: {
     alias: {
@@ -23,22 +45,32 @@ export default defineConfig({
     }
   },
   build: {
+    cssCodeSplit: true,
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'lib/main.ts'),
-      formats: ['es']
+      entry: "lib/components/index.ts",
+      name: "geo-visualisation-components",
+      formats: ["es", "cjs", "umd"],
+      fileName: (format) => `geo-visualisation-components.${format}.js`,
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['vue'],
+      input: {
+        main: path.resolve(__dirname, "lib/main.ts"),
+      },
+      external: ["vue"],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
+        exports: "named",
         globals: {
-          vue: 'Vue',
+          vue: "Vue",
         },
       },
     },
   },
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "lib"),
+  //   },
+  // },
 })
