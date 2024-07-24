15a88bc5d12af95822b332802f7c5d66e0b311a7
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2024-07-24 12:21:19 +1200

    Use viteStaticCopy to copy necessary Cesium files

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,20 @@
 import {resolve} from 'path'
 import {defineConfig} from 'vite'
 import {fileURLToPath} from "node:url";
+import {viteStaticCopy} from "vite-plugin-static-copy";
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [
     vue(),
+    viteStaticCopy({
+      targets: [
+          {src: './node_modules/cesium/Build/Cesium/Assets', dest: './'},
+          {src: './node_modules/cesium/Build/Cesium/ThirdParty', dest: './'},
+          {src: './node_modules/cesium/Build/Cesium/Widgets', dest: './'},
+          {src: './node_modules/cesium/Build/Cesium/Workers', dest: './'},
+      ]
+    }),
   ],
   resolve: {
     alias: {
