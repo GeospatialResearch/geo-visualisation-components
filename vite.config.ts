0be640117c6e47511ca1304b47818515d770252c
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2024-07-29 10:14:06 +1200

    Fix lint errors

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import path from "path";
 import {fileURLToPath} from "node:url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [
     vue(),
